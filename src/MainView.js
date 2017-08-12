0d94c17445e26ed6f3d5c1eb941b704e0055eb88
developer2020 <dev276236@gmail.com>
2017-08-12 15:06:14 +0400

    feat : add Bootstrap css into the project; tried it on the main page

--- src/MainView.js
+++ src/MainView.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './libs/bootstrap/dist/css/bootstrap.min.css'
+import './Readable.css';
 import { serverApiTestMain } from './ServerApiTest'; 
+
 
 class MainView extends Component {
   render() {
     //serverApiTestMain(); 
     return (
-      <div>
-       <h1>hello from main view!</h1>
-      </div>
+     <div className ="container">
+	 <div className ="row">
+	   <div className = "col-md-8 col-md-offset-2 text-center">
+	
+		            <div className = "panel panel-header">
+		      		   <h3>Readable: content and comments</h3>
+		      		</div>
+		
+
+	   </div>
+	   </div>
+	   </div>
     );
   }
 }
