6f809b69dfdd2299d8390c7f52c07a542aebe059
developer2020 <dev276236@gmail.com>
2017-08-29 07:54:42 +0400

    refactor: re-arrange code to segregate common items from post or comment-specific

--- src/components/PostViewSmall.js
+++ src/components/PostViewSmall.js
@@ -4,7 +4,7 @@ import '../libs/bootstrap/dist/css/bootstrap.min.css';
 import './Readable.css';
 import { timeStampToDateAndTime } from '../utils/lib'
 import { connect } from 'react-redux';
-import { deletePost, deleteComment, deleteAllCommentsForPost, upvotePost, upvoteComment, downvotePost, downvoteComment} from "../actions/actions"
+import { deletePost, deleteComment, deleteAllCommentsForPost, upvotePost, upvoteComment, downvotePost, downvoteComment } from "../actions/actions"
 import UpdateComment from "./UpdateComment"
 
 //This component displays post and comments in all views. 
@@ -36,154 +36,215 @@ import UpdateComment from "./UpdateComment"
 //- author 
 //- date and time 
 
+//In any view post or comment are "assembled" from small sub-components; since they are not 
+//relevant to any other module/component and are really small they are all kept in this module. 
+
 
 class PostViewSmall extends Component {
-    state={
-        deletePostConfirmRequested : false, 
-        showEditComment : false
+    constructor() {
+        super();
+
+
+        this.subComponents = {
+            deleteButton: <span><button onClick={this.handleDeleteRequestClick} className="post-footer-button">Delete</button></span>
+        }
+
     }
 
-    handlePostConfirmDeleteClick=()=>{
+    state = {
+        deleteConfirmRequested: false,
+        showEditComment: false
+    }
+
+    handlePostConfirmDeleteClick = () => {
         if (this.props.commentView) {
             this.props.dispatch(deleteComment(this.props.post.id));
         } else {
-            this.props.dispatch(deletePost(this.props.post.id)); 
-            this.props.dispatch(deleteAllCommentsForPost(this.props.post.id)); 
+            this.props.dispatch(deletePost(this.props.post.id));
+            this.props.dispatch(deleteAllCommentsForPost(this.props.post.id));
         }
     }
 
-    handlePostDeleteRequestClick=()=>{
-        this.setState({deletePostConfirmRequested: true})
+    handleDeleteRequestClick = () => {
+        this.setState({ deleteConfirmRequested: true })
     }
 
-    handlePostDeleteRequesCancelClick=()=>{
-        this.setState({deletePostConfirmRequested: false})
+    handleDeleteRequesCancelClick = () => {
+        this.setState({ deleteConfirmRequested: false })
     }
 
-    componentWillUnmount(){
-        this.setState({deletePostConfirmRequested: false})
-    } 
-
-    handleCommentEditCancel=()=>{ 
-        this.setState({showEditComment: false})
+    componentWillUnmount() {
+        this.setState({ deleteConfirmRequested: false })
     }
 
-    handleCommentEditClick=()=>{
-        this.setState({showEditComment: true})
+    handleCommentEditCancel = () => {
+        this.setState({ showEditComment: false })
     }
 
-    handleUpvote=()=>{
-        if (this.props.commentView){
-             this.props.dispatch(upvoteComment(this.props.post.id))
-        }else{
+    handleCommentEditClick = () => {
+        this.setState({ showEditComment: true })
+    }
+
+    handleUpvote = () => {
+        if (this.props.commentView) {
+            this.props.dispatch(upvoteComment(this.props.post.id))
+        } else {
             this.props.dispatch(upvotePost(this.props.post.id))
         }
     }
 
-    handleDownvote=()=>{
-        if (this.props.commentView){
+    handleDownvote = () => {
+        if (this.props.commentView) {
             this.props.dispatch(downvoteComment(this.props.post.id))
-       }else{
-           this.props.dispatch(downvotePost(this.props.post.id))
-       }
+        } else {
+            this.props.dispatch(downvotePost(this.props.post.id))
+        }
     }
 
     render() {
-        let linkToPostDetailedView = null; 
-        let addComment = null; 
-        let editPostButton = null; 
-        let deletePostButton = <span><button onClick={this.handlePostDeleteRequestClick} className="post-footer-button">Delete</button></span>; 
-        let title = this.props.post.title; 
-        let category = <span className="post-category">Category: {this.props.post.category}</span>
-        let comments =  <span className="number-of-comments">Comments: {this.props.post.numberOfComments}</span>
-        let author = <span className="post-author"> By: {this.props.post.author}</span>
+       
 
-        let header  =                 <div className="row post-header">
+        //common between post and comment 
         
-        <div className="col-xs-6 text-left post-sub-header">
-                <span className="text-center post-title">{title}</span>
-                {author}
-            </div>
-        <div className="col-xs-6 text-right  post-sub-header">
-             {category}
-             <span>[{timeStampToDateAndTime(this.props.post.timestamp)}]</span>
-            </div>
-    </div>
-     
-      
-
-        if (this.props.mainView){
-            linkToPostDetailedView = <Link className="post-footer-link" to={"/posts/"+this.props.post.id}>details</Link>; 
-            addComment = <Link className="post-footer-link" to={{pathname:"/posts/"+this.props.post.id, 
-                                                                 query: "addComment"
-                                                                }}>New comment</Link>;
-            deletePostButton = null; 
-        }
-
-        if (this.props.detailedView){
-            addComment = <span><button className="post-footer-button" onClick={this.props.addCommentClickHandler}>Add comment</button></span>
-            editPostButton = <span><button className="post-footer-button" onClick={this.props.handlePostEditOn}>Edit</button></span>
-            
-        }
-      
-        let commentAuthor = null; 
-        let commentTimeAndDate = null; 
-
-        if (this.props.commentView){
-            title = null; 
-            category = null; 
-            comments = null; 
-            header = null; 
-            commentAuthor = <span className="post-author"> Commented by: {this.props.post.author}</span>
-            commentTimeAndDate = <span className="comment-date-time">[{timeStampToDateAndTime(this.props.post.timestamp)}]</span>
-            editPostButton = <span onClick={this.handleCommentEditClick}><button className="post-footer-button">Edit</button></span>
-        }
+        let deleteButton = <span><button onClick={this.handleDeleteRequestClick} className="post-footer-button">Delete</button></span>;
 
         let confirmDelete = <div className="confirm-delete">
-                                 <span><button onClick={this.handlePostConfirmDeleteClick} className="btn btn-default control-style">Confirm delete</button></span>
-                                 <span><button onClick={this.handlePostDeleteRequesCancelClick} className="btn btn-default control-style">Cancel</button></span>
-                            </div>
-        if (this.state.deletePostConfirmRequested){
-            editPostButton = null; 
-            addComment = null; 
-            deletePostButton = null;
+            <span><button onClick={this.handlePostConfirmDeleteClick} className="btn btn-default control-style">Confirm delete</button></span>
+            <span><button onClick={this.handleDeleteRequesCancelClick} className="btn btn-default control-style">Cancel</button></span>
+        </div>
 
-        }else{
-            confirmDelete = null; 
+        let voteScore = <span>
+            <span className="vote-score">Likes: {this.props.post.voteScore}</span>
+            <button className="btn btn-xs vote-button" onClick={this.handleUpvote}><span className="glyphicon glyphicon-arrow-up readables-arrow-button"></span></button>
+            <button className="btn btn-xs vote-button" onClick={this.handleDownvote}><span className="glyphicon glyphicon-arrow-down readables-arrow-button"></span></button>
+        </span>
+
+        if (this.state.showEditComment) {
+            return <UpdateComment handleCancelCommentEdit={this.handleCommentEditCancel} commentId={this.props.post.id}> </UpdateComment>;
         }
 
-        if (this.state.showEditComment){
-			return <UpdateComment handleCancelCommentEdit={this.handleCommentEditCancel} commentId={this.props.post.id}> </UpdateComment>;
-        }
 
-        return (
-
+        //there are three possible scenarios to consider: 
+        //1. this is a comment; 
+        if (this.props.commentView) {
+            let commentAuthor = <span className="post-author"> Commented by: {this.props.post.author}</span>
+            let commentTimeAndDate = <span className="comment-date-time">[{timeStampToDateAndTime(this.props.post.timestamp)}]</span>
+            let editCommentButton = <span onClick={this.handleCommentEditClick}><button className="post-footer-button">Edit</button></span>
+            if (this.state.deleteConfirmRequested) {
+                editCommentButton = null;
+                deleteButton = null;
+    
+            } else {
+                confirmDelete = null;
+            }
+         
+            return (
                 <div className="panel panel-default post-panel">
 
-                {header}
+                    <div>
+                        <pre id="post-body">{this.props.post.body}</pre>
+                    </div>
+                    <div className="row post-footer">
+                        <div className="col-xs-6 text-left post-sub-header">
+                            {voteScore}
+                            {editCommentButton}
+                            {deleteButton}
+                        </div>
+                        <div className="col-xs-6 text-right post-sub-header">
+                            {commentAuthor}
+                            {commentTimeAndDate}
+                        </div>
+
+                    </div>
+                    {confirmDelete}
+                </div>
+            );
+
+        }
+
+
+        //post only 
+       
+        let comments = <span className="number-of-comments">Comments: {this.props.post.numberOfComments}</span>
+      
+        let postHeader = <div className="row post-header">
+
+            <div className="col-xs-6 text-left post-sub-header">
+                <span className="text-center post-title">{this.props.post.title}</span>
+                <span className="post-author"> By: {this.props.post.author}</span>
+            </div>
+            <div className="col-xs-6 text-right  post-sub-header">
+            <span className="post-category">Category: {this.props.post.category}</span>
+                <span>[{timeStampToDateAndTime(this.props.post.timestamp)}]</span>
+            </div>
+        </div>
+
+        //2. post in the main view 
+        if (this.props.mainView) {
+            let linkToPostDetailedView = <Link className="post-footer-link" to={"/posts/" + this.props.post.id}>details</Link>;
+            let linkToAddComment = <Link className="post-footer-link" to={{
+                pathname: "/posts/" + this.props.post.id,
+                query: "addComment"
+            }}>New comment</Link>;
+
+            return (<div className="panel panel-default post-panel">
+
+                {postHeader}
                 <div>
                     <pre id="post-body">{this.props.post.body}</pre>
                 </div>
                 <div className="row post-footer">
                     <div className="col-xs-6 text-left post-sub-header">
-                        <span className="vote-score">Likes: {this.props.post.voteScore}</span>
-                        <button className="btn btn-xs vote-button" onClick={this.handleUpvote}><span className="glyphicon glyphicon-arrow-up readables-arrow-button"></span></button>
-                        <button className="btn btn-xs vote-button" onClick={this.handleDownvote}><span className="glyphicon glyphicon-arrow-down readables-arrow-button"></span></button>
+                        {voteScore}
                         {linkToPostDetailedView}
+                       
+                    </div>
+                    <div className="col-xs-6 text-right post-sub-header">
+                        {comments}
+                        {linkToAddComment}
+                    </div>
+
+                </div>
+            </div>
+            );
+        }
+
+        //3. post in detailed view 
+        if (this.props.detailedView) {
+            let addComment = <span><button className="post-footer-button" onClick={this.props.addCommentClickHandler}>Add comment</button></span>
+            let editPostButton = <span><button className="post-footer-button" onClick={this.props.handlePostEditOn}>Edit</button></span>
+
+            if (this.state.deleteConfirmRequested) {
+                editPostButton = null;
+                addComment = null;
+                deleteButton = null;
+    
+            } else {
+                confirmDelete = null;
+            }
+
+            return (
+               <div className="panel panel-default post-panel">
+                {postHeader}
+                <div>
+                    <pre id="post-body">{this.props.post.body}</pre>
+                </div>
+                <div className="row post-footer">
+                    <div className="col-xs-6 text-left post-sub-header">
+                        {voteScore}
                         {editPostButton}
-                        {deletePostButton}
+                        {deleteButton}
                     </div>
                     <div className="col-xs-6 text-right post-sub-header">
                         {comments}
                         {addComment}
-                        {commentAuthor}
-                        {commentTimeAndDate}
                     </div>
-                        
+
                 </div>
                 {confirmDelete}
             </div>
-        );
+            );
+        }
     }
 }
 
