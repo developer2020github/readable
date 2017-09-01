36f403b8a2194e02632c208210025d7deaa63877
developer2020 <dev276236@gmail.com>
2017-09-01 09:52:47 +0400

    refactor: remove unused code from PostViewSmall (went into comment view)

--- src/components/PostViewSmall.js
+++ src/components/PostViewSmall.js
@@ -9,7 +9,7 @@ import UpdateComment from "./UpdateComment"
 import Vote from "./Vote"
 import EditDeleteButtons from "./EditDeleteButtons"
 
-//This component displays post and comments in all views. 
+//This component displays post  all views. 
 //Post header and body are common for all views. 
 //Post footer can display different links and information depending 
 //on a particular view. 
@@ -24,88 +24,19 @@ import EditDeleteButtons from "./EditDeleteButtons"
 //- button to edit post 
 //- button to delete post 
 //- number of comments 
-//- button to add comments (opens comment form)
-// since differences are rather small, there is no point to create separate component for post 
-// footer. Instead, different elements will be just conditionally rendered depending on what 
-// parent component is asking for. 
-
-//In comment view comment there should be no header
-
-//In comment view comment footer shouls show
-// - voting score
-// - button to edit comment 
-// - button to delete comment 
-//- author 
-//- date and time 
-
-//In any view post or comment are "assembled" from small sub-components; since they are not 
-//relevant to any other module/component and are really small they are all kept in this module. 
-
+//- button to add comments (opens comment form) 
 
 class PostViewSmall extends Component {
 
-
-    state = {
-        deleteConfirmRequested: false,
-        showEditComment: false
-    }
 
     postConfimedDeleteClick = ()=>{
         this.props.dispatch(deletePost(this.props.post.id));
         this.props.dispatch(deleteAllCommentsForPost(this.props.post.id));
     }
 
-    commentConfimedDeleteClick = ()=>{
-        this.props.dispatch(deleteComment(this.props.post.id));
-    }
-  
-    handleCommentEditCancel = () => {
-        this.setState({ showEditComment: false })
-    }
-
-    handleCommentEditClick = () => {
-        this.setState({ showEditComment: true })
-    }
 
     render() {
 
-
-        //there are three possible scenarios to consider: 
-        //1. this is a comment; 
-        
-        if (this.state.showEditComment) {
-            return <UpdateComment handleCancelCommentEdit={this.handleCommentEditCancel} commentId={this.props.post.id}> </UpdateComment>;
-        }
-
-        if (this.props.commentView) {
-            let commentAuthor = <span className="post-author"> Commented by: {this.props.post.author}</span>
-            let commentTimeAndDate = <span className="comment-date-time">[{timeStampToDateAndTime(this.props.post.timestamp)}]</span>
-         
-            return (
-                <div className="panel panel-default post-panel">
-
-                    <div>
-                        <pre id="post-body">{this.props.post.body}</pre>
-                    </div>
-                    <div className="row post-footer">
-                        <div className="col-xs-6 text-left post-sub-header">
-                            <Vote   upvoteAction={upvoteComment} downvoteAction={downvoteComment} voteScore={this.props.post.voteScore} id={this.props.post.id}/>
-                            <EditDeleteButtons handleEditRequest={this.handleCommentEditClick} confimedDeleteAction={this.commentConfimedDeleteClick}/>
-                        </div>
-                        <div className="col-xs-6 text-right post-sub-header">
-                            {commentAuthor}
-                            {commentTimeAndDate}
-                        </div>
-
-                    </div>
-                   
-                </div>
-            );
-
-        }
-
-
-        //post only 
        
         let comments = <span className="number-of-comments">Comments: {this.props.post.numberOfComments}</span>
       
@@ -121,7 +52,7 @@ class PostViewSmall extends Component {
             </div>
         </div>
 
-        //2. post in the main view 
+        //1. post in the main view 
         if (this.props.mainView) {
             let linkToPostDetailedView = <Link className="post-footer-link" to={"/posts/" + this.props.post.id}>details</Link>;
             let linkToAddComment = <Link className="post-footer-link" to={{
@@ -151,7 +82,7 @@ class PostViewSmall extends Component {
             );
         }
 
-        //3. post in detailed view 
+        //2. post in detailed view 
         if (this.props.detailedView) {
             let addComment = <span><button className="post-footer-button" onClick={this.props.addCommentClickHandler}>Add comment</button></span>
 
